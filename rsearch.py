e776f53b8741b00ded3682debdc15ce9f08b3b1f
john doe <scottbruceburton@gmail.com>
2020-08-18 16:29:00 -0400

    added TODOs

---
+++ rsearch.py
@@ -0,0 +1,13 @@
+#! python3
+# Opens all .txt files in a dir and searches for a user supplied expression
+
+# TODO: Get Input From User
+# TODO: 1. Directory Location
+# TODO: 2. Regex String
+
+# TODO: Find all .txt files in directory
+
+# TODO: Loop through each file
+# TODO: Search contents of file for expression
+
+# TODO: Print to Terminal
