48ecd62dd4d3aa42f3fd91a37db1f36b648679a0
john doe <scottbruceburton@gmail.com>
2020-08-19 16:42:16 -0400

    basic working search

--- rsearch.py
+++ rsearch.py
@@ -7,18 +7,31 @@ from pathlib import Path
 # Get Search Phrase From User
 searchPhrase = input("What Word or Phrase would you like to search for?\n")
 
-# TODO: 2. Regex String
+# Regex String
 searchPhrase = re.compile(searchPhrase)
 
 # Get Directory Location
-inFile = input("What File would you like to search for?")
+searchDir = input("What directory would you like to search:\n")
 
-# TODO: a. Validate File Location
+# Validate Directory Location
+while not Path(searchDir).is_dir():
+    print("That is not a valid Directory, try again:\n")
+    searchDir = input()
+
+# create a list of all files
+fileList = os.listdir(searchDir)
+
+# loop through each file
+for f in fileList:
+    # get all text files in directory
+    if f.endswith('.txt'):
+        inFile = Path(searchDir) / f
+        openFile = open(f)
+        fileText = openFile.read()
+        # print(fileText)
+        # show which files have search phrase
+        if searchPhrase.search(fileText):
+            print(f'{f.ljust(20)} contains the phrase you are looking for')
 
 
-# TODO: Find all .txt files in directory
 
-# TODO: Loop through each file
-# TODO: Search contents of file for expression
-
-# TODO: Print to Terminal
