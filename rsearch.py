19bd7e111505de1b8fc442de945884cd258d21f9
john doe <scottbruceburton@gmail.com>
2020-08-19 10:09:51 -0400

    regex added

--- rsearch.py
+++ rsearch.py
@@ -1,9 +1,20 @@
 #! python3
 # Opens all .txt files in a dir and searches for a user supplied expression
 
-# TODO: Get Input From User
-# TODO: 1. Directory Location
+import os, re
+from pathlib import Path
+
+# Get Search Phrase From User
+searchPhrase = input("What Word or Phrase would you like to search for?\n")
+
 # TODO: 2. Regex String
+searchPhrase = re.compile(searchPhrase)
+
+# Get Directory Location
+inFile = input("What File would you like to search for?")
+
+# TODO: a. Validate File Location
+
 
 # TODO: Find all .txt files in directory
 
